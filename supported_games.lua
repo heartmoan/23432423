9a78fb6b7d7e4c9ec1b39f0d3bf67dd2236193d9
Doran342545345 <105158686+Doran342545345@users.noreply.github.com>
2022-10-13 03:51:14 +0200

    Update supported_games.lua

--- supported_games.lua
+++ supported_games.lua
@@ -3,4 +3,5 @@ loadstring(game:HttpGet('https://pastebin.com/raw/dwFkCVyv'))()
 
 --key is in discord: https://discord.gg/PG6ka6ABMw (delete this)
 --[[ Supported games ]]--
---Adopt me
+--Adopt me | https://web.roblox.com/games/920587237/Adopt-Me
+--Roblox but every second you grow | https://web.roblox.com/games/10386702469/TRADING-Roblox-but-every-second-you-grow
