4d6593521c99f751b4abb43dbdce1f61573cb2ea
Doran342545345 <105158686+Doran342545345@users.noreply.github.com>
2022-10-26 16:18:54 +0200

    Update supported_games.lua

--- supported_games.lua
+++ supported_games.lua
@@ -6,3 +6,4 @@ loadstring(game:HttpGet('https://pastebin.com/raw/dwFkCVyv'))()
 --Adopt me | https://web.roblox.com/games/920587237/Adopt-Me
 --Roblox but every second you grow | https://web.roblox.com/games/10386702469/TRADING-Roblox-but-every-second-you-grow
 --Combat Warriors | https://web.roblox.com/games/4282985734/Combat-Warriors
+--Murder Mystery 2
